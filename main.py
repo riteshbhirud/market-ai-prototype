b3e44b033584e95bec441dfaa2fc894ec5056871
natan <72627669+natkai2040@users.noreply.github.com>
2026-04-10 18:06:17 -0400

    Initial Commit
    Created main.py and example_data.json

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+# import json
+from pyproj import CRS
+# import requests
+from pydantic import BaseModel
+# import os
+from typing import List
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # change to your Vercel domain later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#####################################################################
+
+class Listing(BaseModel):
+    listing_type: str | None = None
+    condition: str | None = None
+    description: str | None = None
+    price: float | None = None
+    grade: str | None = None
+    platform: str | None = None
+
+class MarketplaceItem(BaseModel):
+    item_name: str
+    listings: List[Listing]
+
+print("AI Interpreter Loaded")
+
+@app.post("/interpret/")
+async def interpret(item: MarketplaceItem):
+    return {"message": "received item " + item.item_name}
+
